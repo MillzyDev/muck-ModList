4aaf5c8b611ab53fd052c164b52a9a4e532cdc72
Millzy <millzyyt08@gmail.com>
2022-02-23 17:21:12 +0000

    Button util

---
+++ ModList/UIUtils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace ModList
+{
+    static class UIUtils
+    {
+        public static Button CreateButton(Transform parent, string text, UnityAction callback, string template = "MultiplayerButton")
+        {
+            GameObject prefab = GameObject.Find(template);
+
+            GameObject buttonObj = UnityEngine.Object.Instantiate(prefab);
+            buttonObj.name = "ModListButton";
+            buttonObj.transform.SetParent(parent);
+
+            var modListText = buttonObj.GetComponentInChildren<TextMeshProUGUI>();
+            modListText.text = text;
+
+            var button = buttonObj.GetComponent<Button>();
+            button.onClick = new Button.ButtonClickedEvent();
+            button.onClick.AddListener(callback);
+
+            return button;
+        }
+    }
+}
