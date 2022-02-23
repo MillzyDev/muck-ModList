c76f2ccfd368ce1960ce70a60d310a00d9c6c193
Millzy <millzyyt08@gmail.com>
2022-02-23 17:21:19 +0000

    plugin.cs

---
+++ ModList/Plugin.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace ModList
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, "ModList", PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static Plugin instance;
+        public static Harmony harmonyInstance;
+        public static ManualLogSource logger;
+
+        private void Awake()
+        {
+            instance = this;
+            harmonyInstance = new Harmony("dev.MillzyG.muck-ModList");
+            logger = Logger;
+
+            harmonyInstance.PatchAll();
+
+            // Plugin startup logic
+            logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
