44d00011f95450ed0876e340252665c4f7e899fd
Millzy <millzyyt08@gmail.com>
2022-02-24 21:36:29 +0000

    made ui better

--- ModList/HarmonyPatches/MenuUI.cs
+++ ModList/HarmonyPatches/MenuUI.cs
@@ -1,9 +1,13 @@
 ﻿using BepInEx.Bootstrap;
 using HarmonyLib;
+using ModList.UI;
+using System;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
+using static ModList.Plugin;
 
 namespace ModList.HarmonyPatches
 {
@@ -13,19 +17,14 @@ namespace ModList.HarmonyPatches
     {
         static GameObject view;
         static GameObject main;
+        static TextMeshProUGUI modsText;
+        static int pageTotal;
+        static Button nextButton;
+        static Button prevButton;
+        static TextMeshProUGUI pageCount;
 
         private static void Postfix(global::MenuUI __instance)
         {
-            Plugin.logger.LogInfo("Getting loaded mods");
-            // creating our text
-            string mods = "\n     <size=42><u>Loaded Mods</u></size>\n";
-
-            var pluginInfos = Chainloader.PluginInfos;
-            foreach (var pluginInfo in pluginInfos)
-            {
-                mods += $"     {pluginInfo.Key}\n";
-            }
-
             Plugin.logger.LogInfo("Getting main UI object");
             main = GameObject.Find("UI/Main");
 
@@ -39,41 +38,48 @@ namespace ModList.HarmonyPatches
 
             // create a new canvas for our ui (the other ui object removes ours fsr)
             Plugin.logger.LogInfo("Creating canvas");
-            GameObject canvas = new GameObject("ModListCanvas", typeof(RectTransform));
-            Canvas canv = canvas.AddComponent<Canvas>();
-            canv.renderMode = RenderMode.ScreenSpaceCamera;
-            canvas.AddComponent<GraphicRaycaster>();
+            GameObject canvas = UIUtils.CreateCanvas(RenderMode.ScreenSpaceCamera);
+            //Object.DontDestroyOnLoad(canvas);
 
-            // get the credits thing, cuz its easier
-            Plugin.logger.LogInfo("Instantiating credits");
-            view = Object.Instantiate(FindInactiveObject("Credits"));
-            view.transform.SetParent(canvas.transform);
-            RemoveComponent<Credits>(view); // remove this; we dont need it
-            //view.SetActive(true);
-            view.transform.localPosition = new Vector3(0f, 0f, 0f); // fix position
+            GameObject container = UIUtils.CreateView(canvas.transform, "ModList - Loaded Plugins");
+            view = container.transform.parent.parent.gameObject;
 
-            // disable cringe stuff we dont need
-            Plugin.logger.LogInfo("Removing credits");
-            Transform credits = view.transform.Find("GameplaySettings/RawImage (1)");
-            credits.gameObject.SetActive(false);
+            GameObject mods = new GameObject("ModListLoadedPlugins");
+            mods.AddComponent<VerticalLayoutGroup>();
+            mods.transform.SetParent(container.transform);
+            mods.GetComponent<RectTransform>().anchoredPosition = new Vector2(360f, -250f);
 
-            Plugin.logger.LogInfo("Removing buttons");
-            Transform thingyWeUse = view.transform.Find("GameplaySettings/RawImage");
-            Transform button2 = thingyWeUse.Find("Button (2)");
-            button2.gameObject.SetActive(false);
-            Transform button3 = thingyWeUse.Find("Button (3)");
-            button3.gameObject.SetActive(false);
+            List<string> pluginNames = new List<string>();
+            List<string> pluginIds;
+            foreach (var info in Chainloader.PluginInfos)
+            {
+                string key = info.Key;
+                string[] nameSpaces = key.Split('.');
+                string name = nameSpaces[nameSpaces.Length - 1];
 
-            Plugin.logger.LogInfo("Setting text");
-            TextMeshProUGUI text = thingyWeUse.GetComponentInChildren<TextMeshProUGUI>();
-            text.alignment = TextAlignmentOptions.TopLeft;
-            text.fontSize = 25f;
-            text.text = mods;
+                pluginNames.Add($"<color=green>{name}</color>");
+            }
+            string text = pluginNames.Join(delimiter: "\n");
+            logger.LogInfo($"Found a total of {pluginNames.Count} plugins!");
+            pageTotal = (int)Math.Ceiling(pluginNames.Count / 13d);
 
-            // fix button pos
-            Plugin.logger.LogInfo("Moving back button");
-            GameObject backButton = view.transform.Find("MenuButton").gameObject;
-            backButton.transform.localPosition = new Vector3(-375f, 350f, 0f);
+            modsText = UIUtils.CreateText(mods.transform, text);
+            modsText.overflowMode = TextOverflowModes.Page;
+
+            GameObject pages = new GameObject("ModListPageSelection");
+            var pagesLayout = pages.AddComponent<HorizontalLayoutGroup>();
+            var pagesLayoutElement = pages.AddComponent<LayoutElement>();
+            pages.transform.SetParent(container.transform);
+            pagesLayout.GetComponent<RectTransform>();
+            pagesLayoutElement.minHeight = 65f;
+
+            prevButton = UIUtils.CreateButton(pages.transform, "< Prev", PrevPage);
+            prevButton.interactable = false;
+            pageCount = UIUtils.CreateText(pages.transform, $"{modsText.pageToDisplay}/{pageTotal}");
+            pageCount.alignment = TextAlignmentOptions.Midline;
+            pageCount.fontSize = 35f;
+            nextButton = UIUtils.CreateButton(pages.transform, "Next >", NextPage);
+            nextButton.interactable = pageTotal > 1;
         }
 
         private static void OnModsClick()
@@ -82,28 +88,41 @@ namespace ModList.HarmonyPatches
             main?.SetActive(false);
         }
 
-        private static GameObject FindInactiveObject(string name)
+        private static void NextPage()
         {
-            Transform[] trs = Resources.FindObjectsOfTypeAll<Transform>();
-            foreach (Transform t in trs)
+            modsText.pageToDisplay++;
+            if (modsText.pageToDisplay == pageTotal)
             {
-                if (t.name == name)
-                {
-                    return t.gameObject;
-                }
+                nextButton.interactable = false;
+                prevButton.interactable = true;
             }
-            return null;
-        }
-        private static void RemoveComponent<Component>(GameObject go)
-        {
-            Component component = go.GetComponent<Component>();
-
-            if (component != null)
+            else
             {
-                Object.DestroyImmediate(component as Object, true);
-
+                nextButton.interactable = true;
+                prevButton.interactable = true;
             }
+            UpdatePageCount();
         }
 
+        private static void PrevPage()
+        {
+            modsText.pageToDisplay--;
+            if (modsText.pageToDisplay == 1)
+            {
+                prevButton.interactable = false;
+                nextButton.interactable = true;
+            }
+            else
+            {
+                prevButton.interactable = true;
+                nextButton.interactable = true;
+            }
+            UpdatePageCount();
+        }
+
+        private static void UpdatePageCount()
+        {
+            pageCount.text = $"{modsText.pageToDisplay}/{pageTotal}";
+        }
     }
 }
