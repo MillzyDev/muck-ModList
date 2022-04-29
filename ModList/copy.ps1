630a9017e58b79a1cc683722137dfae459ff91ca
Millzy <millzyyt08@gmail.com>
2022-04-29 23:20:57 +0100

    fix script

--- ModList/copy.ps1
+++ ModList/copy.ps1
@@ -1,2 +1,4 @@
 ﻿dotnet build ModList.csproj
-Copy-Item "C:\Users\USER\Documents\GitHub\muck-ModList\ModList\bin\Debug\net472\dev.MillzyG.muck-ModList.dll" -Destination "C:\Users\USER\AppData\Roaming\Thunderstore Mod Manager\DataFolder\Muck\profiles\Default\BepInEx\plugins"
+Copy-Item "C:\Users\USER\Documents\GitHub\muck-ModList\ModList\bin\Debug\net472\dev.Millzy.Muck.ModList.dll" -Destination "C:\Users\USER\AppData\Roaming\Thunderstore Mod Manager\DataFolder\Muck\profiles\Default\BepInEx\plugins"
+Copy-Item "C:\Users\USER\Documents\GitHub\muck-ModList\ModList\bin\Debug\net472\Newtonsoft.Json.dll" -Destination "C:\Users\USER\AppData\Roaming\Thunderstore Mod Manager\DataFolder\Muck\profiles\Default\BepInEx\plugins"
+Copy-Item "C:\Users\USER\Documents\GitHub\muck-ModList\ModList\bin\Debug\net472\dev.Millzy.Muck.ModList.pdb" -Destination "C:\Users\USER\AppData\Roaming\Thunderstore Mod Manager\DataFolder\Muck\profiles\Default\BepInEx\plugins"
