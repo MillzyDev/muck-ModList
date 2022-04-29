8440446b1e0ceef8d0baf8f7726458aa1feda1ee
Millzy <millzyyt08@gmail.com>
2022-04-29 23:22:42 +0100

    getting links working

---
+++ ModList/Components/Hyperlink.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace ModList.Components
+{
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    class Hyperlink : MonoBehaviour, IPointerClickHandler
+    {
+        TextMeshProUGUI text;
+
+        private string url = string.Empty;
+        public string URL
+        {
+            get => url;
+            set
+            {
+                url = value == null ? "https://muck.thunderstore.io/" : value;
+
+                try
+                {
+                    text.text = $"Website ({new Uri(url).Host})";
+                }
+                catch
+                {
+                    text.text = $"Website (???)";
+                }
+            }
+        }
+
+        void Start()
+        {
+            text = GetComponent<TextMeshProUGUI>();
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            Application.OpenURL(URL);
+        }
+    }
+}
