09ad929006358ec01f1e3d4b881ed69b625cbacb
Millzy <millzyyt08@gmail.com>
2022-04-29 23:21:33 +0100

    serializable manifest

---
+++ ModList/PluginManifest.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+
+namespace ModList
+{
+    public class PluginManifest
+    {
+        [JsonProperty("name")]
+        public string Name
+        {
+            get;
+            private set;
+        }
+
+        [JsonProperty("version_number")]
+        public string Version
+        {
+            get;
+            private set;
+        }
+
+        [JsonProperty("website_url")]
+        public string Website
+        {
+            get;
+            private set;
+        }
+
+        [JsonProperty("description")]
+        public string Description
+        {
+            get;
+            private set;
+        }
+
+        [JsonProperty("dependencies")]
+        public string[] Dependencies
+        {
+            get;
+            private set;
+        }
+    }
+}
