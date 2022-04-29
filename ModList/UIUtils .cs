cd502ec52618b2450315c3440adb069509d4eba4
Millzy <millzyyt08@gmail.com>
2022-04-29 23:21:47 +0100

    reduce code ui

---
+++ ModList/UIUtils .cs
@@ -0,0 +1,26 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ModList
+{
+    static class UIUtils
+    {
+        public static Button CreateButton(Transform parent, string text, string template = "MultiplayerButton")
+        {
+            GameObject prefab = GameObject.Find(template);
+
+            GameObject buttonObj = UnityEngine.Object.Instantiate(prefab);
+            buttonObj.name = "ModListButton";
+            buttonObj.transform.SetParent(parent);
+
+            var modListText = buttonObj.GetComponentInChildren<TextMeshProUGUI>();
+            modListText.text = text;
+
+            var button = buttonObj.GetComponent<Button>();
+            button.onClick = new Button.ButtonClickedEvent();
+
+            return button;
+        }
+    }
+}
