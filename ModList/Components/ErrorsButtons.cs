ca70d8ddace36add3a363eb3b7b358a1a0a8d5d8
Millzy <millzyyt08@gmail.com>
2022-04-29 23:22:35 +0100

    buttons

---
+++ ModList/Components/ErrorsButtons.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace ModList.Components
+{
+    [RequireComponent(typeof(ModListUI))]
+    public class ErrorsButtons : MonoBehaviour
+    {
+        ModListUI modListUI;
+
+        void Start()
+        {
+            modListUI = GetComponent<ModListUI>();
+            modListUI.ErrorsBackButton.onClick.AddListener(BackWasPressed);
+        }
+
+        void BackWasPressed()
+        {
+            modListUI.ErrorsView.SetActive(false);
+            modListUI.ModListView.SetActive(true);
+        }
+    }
+}
