f64f64f1a05bb4d4beedd7c62b42eb047b4b5e76
Millzy <millzyyt08@gmail.com>
2023-08-14 18:58:51 +0100

    back button fix

---
+++ ModList/ModListBackButton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace ModList
+{
+    public class ModListBackButton : MonoBehaviour
+    {
+        public Button[] backButtons = null!;
+
+        private void Start()
+        {
+            var menuUi = GetComponentInParent<MenuUI>();
+            GameObject mainUi = menuUi.mainUi;
+            
+            foreach (Button button in backButtons)
+            {
+                button.onClick = new Button.ButtonClickedEvent();
+                button.onClick.AddListener(() =>
+                {
+                    gameObject.SetActive(false);
+                    mainUi.SetActive(true);
+                });
+            }
+        }
+    }
+}
---
+++ UnityAssets/Assets/ModList/ModListBackButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace ModList
+{
+    public class ModListBackButton : MonoBehaviour
+    {
+        public Button[] backButtons;
+
+        private void Start()
+        {
+        }
+    }
+}
