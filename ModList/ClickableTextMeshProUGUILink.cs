d7535bf4c44e004f015d11493b78573588be7559
Millzy <millzyyt08@gmail.com>
2023-08-14 18:59:07 +0100

    open website

---
+++ ModList/ClickableTextMeshProUGUILink.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace ModList
+{
+    public class ClickableTextMeshProUGUILink : TextMeshProUGUI, IPointerClickHandler
+    {
+        private string _url = string.Empty;
+
+        public string Url
+        {
+            set
+            {
+                if (value == _url)
+                    return;
+                
+                var uri = new Uri(value);
+                string tld = uri.Host;
+
+                text = "Website (" + tld + ")";
+                
+                _url = value;
+            }
+        }
+
+        protected override void Start()
+        {
+            base.Start();
+
+            text = "";
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            Application.OpenURL(_url);
+        }
+    }
+}
