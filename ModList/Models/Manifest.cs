9e0b4b8ed66821058d32c076d733e45f82091703
Millzy <millzyyt08@gmail.com>
2023-08-15 09:27:23 +0100

    manifest model

---
+++ ModList/Models/Manifest.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace ModList.Models
+{
+    public class Manifest
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; } = string.Empty;
+        
+        [JsonProperty("description")]
+        public string Description { get; set; } = string.Empty;
+
+        [JsonProperty("version_number")]
+        public string VersionNumber { get; set; } = "0.0.0";
+
+        [JsonProperty("dependencies")]
+        public string[] Dependencies { get; set; } = {};
+
+        [JsonProperty("website_url")]
+        public string WebsiteUrl { get; set; } = string.Empty;
+    }
+}
